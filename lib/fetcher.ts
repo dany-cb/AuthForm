f360c82f9d413d052a4b3812beb20958ccc44b6b
Daniel Thomas <danjes002@gmail.com>
2022-05-25 22:06:43 +0530

    feat: add fetch helper

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,17 +1,27 @@
-export default (url: string, data?: object) => {
-  return fetch(`${window.location.origin}/api${url}`, {
+const fetcher = (url: string, data?: object) => {
+  let fetchData = fetch(`${window.location.origin}/api${url}`, {
     method: data ? 'POST' : 'GET',
     credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
+  }).then((res) => {
+    if (!res.ok) {
+      throw res
+    }
+    return res.json()
   })
-    .then((res) => {
-      if (!res.ok) {
-        throw res
-      }
-      return res.json()
-    })
-    .catch((err) => console.error(err))
+
+  return {
+    then(callback: (data: object) => void) {
+      fetchData = fetchData.then(callback)
+      return this
+    },
+    catch: (errorCallback: (error) => void) => {
+      fetchData.catch((err) => err.json()).then(errorCallback)
+    },
+  }
 }
+
+export default fetcher
