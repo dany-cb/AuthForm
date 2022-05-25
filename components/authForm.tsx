204ee52c947a199c2902ccba9864281104c6950f
Daniel Thomas <danjes002@gmail.com>
2022-05-25 22:08:45 +0530

    fix: separate FIELDS, add onsubmit

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -9,73 +9,15 @@ import {
   Stack,
 } from '@chakra-ui/react'
 import InputField from '../components/inputField'
-import { FC, useEffect, useState } from 'react'
+import { FC, useState } from 'react'
 import fetcher from '../lib/fetcher'
-
-const REGEX = {
-  username: /^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-  email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-  password: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-}
-
-const FIELDS = {
-  signin: [
-    {
-      id: 'user',
-      label: 'Username/Email',
-      type: 'text',
-      required: true,
-      pattern: {
-        value: new RegExp(`(${REGEX.username.source})|(${REGEX.email.source})`),
-        message: 'Invalid',
-      },
-    },
-    {
-      id: 'pwd',
-      label: 'Password',
-      type: 'password',
-      required: true,
-    },
-  ],
-  signup: [
-    {
-      id: 'username',
-      label: 'Username',
-      type: 'text',
-      required: true,
-      pattern: REGEX.username,
-    },
-    {
-      id: 'email',
-      label: 'Email',
-      type: 'email',
-      required: true,
-      pattern: REGEX.email,
-    },
-    // Nested array for Horizontal-Stacking
-    [
-      {
-        id: 'pwd',
-        label: 'Password',
-        type: 'password',
-        pattern: {
-          value: REGEX.password,
-          message:
-            'Minimum eight characters, at least one letter, one number and one special character',
-        },
-        required: true,
-      },
-      {
-        id: 'confirmPwd',
-        label: 'Confirm',
-        type: 'password',
-        required: false,
-      },
-    ],
-  ],
-}
+import FIELDS from '../utils/authData'
+import { useRouter } from 'next/router'
+import { errorData } from '../types'
 
 const AuthForm: FC<{ mode: 'signin' | 'signup' }> = ({ mode }) => {
+  const router = useRouter()
+
   /**
    * Returns value based on the current mode value
    * @returns signInVal | signUpVal
@@ -104,6 +46,9 @@ const AuthForm: FC<{ mode: 'signin' | 'signup' }> = ({ mode }) => {
   const submitHandler = (data) => {
     setSubmit(true)
     fetcher(picker('/signin', '/signup'), data)
+      .then(() => router.push('/'))
+      .catch((err: errorData) => console.log('Error Message:', err.title))
+
     setTimeout(() => setSubmit(false), 5000)
   }
 
